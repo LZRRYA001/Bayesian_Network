4d2a59c1bd9c1c46e080dc133c0ea8d2aba25375
Josh <rdljos002@myuct.ac.za>
2019-09-27 11:59:13 +0200

    Add main functionality to AbstractAnalyser

--- Java Project/src/AbsractAnalyser.java
+++ Java Project/src/AbsractAnalyser.java
@@ -1,101 +1,159 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 class AbstractAnalyser {
 	
-	public static openFiles(String directory) {
+	private static int countAI; //the number of CSC words counted
+	private static HashMap<String, Integer> groupCounts; //store the count of words present in a group
+	
+	private static ArrayList<String> rows;
+	
+	// populate keyword group ArrayLists
+	// NB! The first item must be the name of the group
+	
+	// Logic / Ontologies/ causal networks/ knowledge based systems
+	private static ArrayList<String> A = new ArrayList<>(Arrays.asList("A", "logics", "ontology", "casual", "networks", "knowledge",
+			"based", "systems", "architecture", "bayesian", "causal", "semantic", "logic", "belief", "beliefs",
+			"klm", "defeasible", "entailment", "rational", "closure", "semantics", "subsumption", "description",
+			"dls", "network", "system", "alc", "monotonic", "nmr", "classical", "preferential", "typicality",
+			"nonmonotonic", "ontologies", "lexicographic", "owl", "languages", "grammar"));
+	// Representing and reasoning with expert knowledge
+	private static ArrayList<String> B = new ArrayList<>(
+			Arrays.asList("B", "represent", "representing", "reason", "expert", "knowledge", "reasoning", "intelligence",
+					"representation", "verbalisation", "design", "capture", "domains"));
+	// Top down AI
+	private static ArrayList<String> C = new ArrayList<>(
+			Arrays.asList("C","top", "down", "ai", "algorithm", "algorithms", "controllers", "controller"));
+	// artificial neural networks, clustering techniques
+	private static ArrayList<String> D = new ArrayList<>(Arrays.asList("D","artificial", "networks", "clustering", "technique",
+			"cluster", "decision", "network", "decisions", "hybrid", "artificial", "modularity", "collective",
+			"cone", "neural", "neuro", "web"));
+	// Building classification and predictive models from labelled historical data
+	private static ArrayList<String> E = new ArrayList<>(Arrays.asList("E","build", "building", "predictive", "model", "models",
+			"modelled", "label", "labelled", "historical", "history", "historically", "factors", "classification",
+			"observations", "predict", "probabilistic", "analytics", "scheduling"));
+	// Finding structure or patterns in unlabelled data
+	private static ArrayList<String> F = new ArrayList<>(Arrays.asList("F","find", "finding", "structure", "unlabelled", "data",
+			"environments", "environment", "mining", "patterns", "emergent"));
+	// Bottom up AI
+	private static ArrayList<String> G = new ArrayList<>(Arrays.asList("G","bottom", "ip", "ai", "data", "learning"));
+	// Reinforcement learning, Markov decision processes
+	private static ArrayList<String> H = new ArrayList<>(Arrays.asList("H","reinforcement", "learning", "markov", "decision",
+			"process", "processes", "convexity", "interactions", "machine", "adaptive", "stochastic", "behavioural",
+			"evolution", "evolutionary", "genotype", "generations"));
+	// Models for action/ path selection in complex environments
+	private static ArrayList<String> I = new ArrayList<>(Arrays.asList("I","models", "model", "action", "path", "select", "selection",
+			"complex", "environment", "environments", "agent", "behaviour", "situations", "control", "autonomous",
+			"modelling", "metamodel", "modules", "modelling", "behaviour", "behaviours", "cooperation",
+			"cooperative", "robot", "robots", "teams", "traffic", "simulated", "simulation", "multiagent"));
+	
+	//Add all groups of words to an ArrayList
+	@SuppressWarnings("serial")
+	private static ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>() {
+		{
+			add(A);
+			add(B);
+			add(C);
+			add(D);
+			add(E);
+			add(F);
+			add(G);
+			add(H);
+			add(I);
+		}
+	};
+
+	public static void openFiles(String directory) {
 		File dir = new File(directory);
 		File[] directoryListing = dir.listFiles();
 		if (directoryListing != null) {
 			for (File child : directoryListing) {
-				// Do something with child
+				readFile(child);
+				generateRow(directory);
 			}
 		} else {
 			// Handle the case where dir is not really a directory.
-			System.out.println("Directory \"" + directory + "\" does not exist." );
+			System.out.println("Directory \"" + directory + "\" does not exist.");
 		}
 	}
 
-	public static void main(String[] args) {
-
-		// read in abstract to be processed
-		ArrayList<String> deshen = new ArrayList<String>();
-		ArrayList<String> geoff = new ArrayList<String>();
-		ArrayList<String> maria = new ArrayList<String>();
-		ArrayList<String> tommie = new ArrayList<String>();
-		ArrayList<String> hussein = new ArrayList<String>();
-
-		Scanner input;
-		/*
-		 * try { input = new Scanner(new File("deshen.txt")); while
-		 * (input.hasNextLine()){ deshen.add(input.nextLine());}//end while input = new
-		 * Scanner(new File("geoff.txt")); while (input.hasNextLine()){
-		 * geoff.add(input.nextLine());}//end while input = new Scanner(new
-		 * File("maria.txt")); while (input.hasNextLine()){
-		 * maria.add(input.nextLine());}//end while input = new Scanner(new
-		 * File("tommie.txt")); while (input.hasNextLine()){
-		 * tommie.add(input.nextLine());}//end while input = new Scanner(new
-		 * File("hussein.txt")); while
-		 * (input.hasNextLine()){hussein.add(input.nextLine());}//end while
-		 * input.close(); } catch (Exception e) {System.out.println(e);}
-		 */
-
-		
-
-		// init an arraylist for each keyword group / concept node title
-
-		// Logic / Ontologies/ causal networks/ knowledge based systems
-		ArrayList<String> A = new ArrayList<>(Arrays.asList("logics", "ontology", "casual", "networks", "knowledge",
-				"based", "systems", "architecture", "bayesian", "causal", "semantic", "logic", "belief", "beliefs",
-				"klm", "defeasible", "entailment", "rational", "closure", "semantics", "subsumption", "description",
-				"dls", "network", "system", "alc", "monotonic", "nmr", "classical", "preferential", "typicality",
-				"nonmonotonic", "ontologies", "lexicographic", "owl", "languages", "grammar"));
-		// Representing and reasoning with expert knowledge
-		ArrayList<String> B = new ArrayList<>(
-				Arrays.asList("represent", "representing", "reason", "expert", "knowledge", "reasoning", "intelligence",
-						"representation", "verbalisation", "design", "capture", "domains"));
-		// Top down AI
-		ArrayList<String> C = new ArrayList<>(
-				Arrays.asList("top", "down", "ai", "algorithm", "algorithms", "controllers", "controller"));
-		// artificial neural networks, clustering techniques
-		ArrayList<String> D = new ArrayList<>(Arrays.asList("artificial", "networks", "clustering", "technique",
-				"cluster", "decision", "network", "decisions", "hybrid", "artificial", "modularity", "collective",
-				"cone", "neural", "neuro", "web"));
-		// Building classification and predictive models from labelled historical data
-		ArrayList<String> E = new ArrayList<>(Arrays.asList("build", "building", "predictive", "model", "models",
-				"modelled", "label", "labelled", "historical", "history", "historically", "factors", "classification",
-				"observations", "predict", "probabilistic", "analytics", "scheduling"));
-		// Finding structure or patterns in unlabelled data
-		ArrayList<String> F = new ArrayList<>(Arrays.asList("find", "finding", "structure", "unlabelled", "data",
-				"environments", "environment", "mining", "patterns", "emergent"));
-		// Bottom up AI
-		ArrayList<String> G = new ArrayList<>(Arrays.asList("bottom", "ip", "ai", "data", "learning"));
-		// Reinforcement learning, Markov decision processes
-		ArrayList<String> H = new ArrayList<>(Arrays.asList("reinforcement", "learning", "markov", "decision",
-				"process", "processes", "convexity", "interactions", "machine", "adaptive", "stochastic", "behavioural",
-				"evolution", "evolutionary", "genotype", "generations"));
-		// Models for action/ path selection in complex environments
-		ArrayList<String> I = new ArrayList<>(Arrays.asList("models", "model", "action", "path", "select", "selection",
-				"complex", "environment", "environments", "agent", "behaviour", "situations", "control", "autonomous",
-				"modelling", "metamodel", "modules", "modelling", "behaviour", "behaviours", "cooperation",
-				"cooperative", "robot", "robots", "teams", "traffic", "simulated", "simulation", "multiagent"));
-		ArrayList<ArrayList> groups = new ArrayList<ArrayList>() {
-			{
-				add(A);
-				add(B);
-				add(C);
-				add(D);
-				add(E);
-				add(F);
-				add(G);
-				add(H);
-				add(I);
+	public static void readFile(File file) {
+		Scanner s;
+		try {
+			s = new Scanner(file);
+			// Iterate through lines
+			while (s.hasNextLine()) {
+				String line = s.nextLine();
+				String afterchars = line.replaceAll("[.\"!,?\'@#$%&*(\")-+^:<>-]*", "").toLowerCase();
+            	System.out.println(afterchars);
+                String[] words = afterchars.split(" ");//those are your words
+                for(String word : words) {
+                	classifyWord(word);
+                }
 			}
-		};
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void classifyWord(String word) {
+		boolean isAIWord = false;
+		for(ArrayList<String> group : groups) {
+			if (group.contains(word)){
+				String groupName = group.get(0);
+				Integer currentCount = groupCounts.get(groupName);
+				groupCounts.put(groupName, currentCount++); // update count
+				isAIWord = true;
+			}
+		}
+		if(isAIWord) {
+			countAI++; //update AI count
+		}
+	}
+	
+	public static void generateRow(String profName) {
+		String[] groups = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+		String row = "Prof_" + profName; //add prof name as first column
+		for(String group: groups) {
+			float percent = groupCounts.get(group)/ countAI;
+			if(percent <= 0.33) {
+				row = row + "\"Low_Focus\"";
+			}else if(percent <= 0.66) {
+				row = row + "\"Low_Focus\"";
+			}else {
+				row = row + "\"High_Focus\"";
+			}
+			row = row + "\t";
+		}
+		row = row + "*\t*\t*\t*"; //add * for empty values
+		rows.add(row);
+		System.out.println(row);
+		
+		//clear counts for next row
+		reset();
+		
+	}
+	
+	//Clear counts in HashMap to start counting from 0
+	public static void reset() {
+		countAI = 0;
+		groupCounts.put("A", 0); groupCounts.put("B", 0); groupCounts.put("C", 0);
+		groupCounts.put("D", 0); groupCounts.put("E", 0); groupCounts.put("F", 0);
+		groupCounts.put("G", 0); groupCounts.put("H", 0); groupCounts.put("I", 0);
+	}
 
-		// populate keyword group arraylists
+	public static void main(String[] args) {
+		
+		//Initialise HashMap & countAI values
+		reset();
+
+		//Initialise ArrayList to store all rows to write to case file
+		rows = new ArrayList<String>();
+		
 
 		// prep abstract for processing
 
